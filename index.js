a4bd6efdf64efd9faa05f8864c6bdabfc1169ea4
LOI PHAT DINH <liamphatdinh@gmail.com>
2018-10-08 05:30:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,3 +6,23 @@ function whisper(string) {
   return string.toLowerCase()
 }
 
+function logShout(string) {
+  console.log(string.toUpperCase())
+}
+
+function logWhisper(string) {
+  console.log(string.toLowerCase())
+}
+
+function sayHiToGrandma(string) {
+  lowercase = string
+  if (lowercase.toLowerCase() === string) {
+    return "I can't hear you!"
+  }
+  else if (lowercase.toUpperCase() === string) {
+    return "YES INDEED!"
+  }
+  else if (lowercase === "I love you, Grandma"){
+    return "I love you, too."
+  }
+}
